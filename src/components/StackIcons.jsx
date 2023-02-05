c2b79f0c58d0f34b50af5b8e6ef91abd0ebf1b2e
Daniel Dimitrov <79416039+ddimitrov1108@users.noreply.github.com>
2023-02-05 11:53:37 +0200

    ui stack fixes

--- src/components/StackIcons.jsx
+++ src/components/StackIcons.jsx
@@ -26,13 +26,13 @@ export default function StackIcons() {
       <SiRedux className="scale-md absolute top-[12%] right-[16%] text-purple-900" />
       <SiTypescript className="scale-xs absolute top-[28%] right-[6%] text-blue-900" />
       <FaNodeJs className="scale-sm absolute top-[30%] left-[36%] text-green-900" />
-      <SiTailwindcss className="scale-lg absolute top-[42%] left-[23%] text-cyan-400" />
+      <SiTailwindcss className="scale-lg absolute top-[60%] left-[29%] text-cyan-400" />
       <TbCSharp className="scale-sm absolute top-[75%] left-[42%] text-purple-700" />
       <SiMysql className="scale-sm absolute top-[16%] left-[48%] text-primary-dark" />
       <SiJquery className="scale-xs absolute top-[74%] right-[11%] text-blue-500" />
       <SiMongodb className="scale-lg absolute top-[48%] right-[20%] text-green-600" />{" "}
       <SiJavascript className="scale-sm absolute top-[65%] right-[30%] bg-black text-yellow-400" />
-      <SiCss3 className="scale-xs absolute top-[60%] left-[29%] text-primary-light" />
+      <SiCss3 className="scale-xs absolute top-[42%] left-[23%] text-primary-light" />
       <SiBootstrap className="scale-xs absolute top-[32%] right-[30%] text-purple-500" />
       <SiHtml5 className="scale-sm absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 text-orange-600" />
     </div>
