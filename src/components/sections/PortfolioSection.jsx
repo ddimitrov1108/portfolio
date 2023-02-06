32f5dc7cc43a590663b69168fed7c21b197adf42
Daniel Dimitrov <79416039+ddimitrov1108@users.noreply.github.com>
2023-02-06 12:28:11 +0200

    .

--- src/components/sections/PortfolioSection.jsx
+++ src/components/sections/PortfolioSection.jsx
@@ -1,26 +1,21 @@
 import SectionHeader from "./components/SectionHeader";
+import SectionFooter from "./components/SectionFooter";
+import SectionMain from "./components/SectionMain";
 
 export default function PortfolioSection() {
   return (
-    <div id="portfolio" className="h-screen flex items-start py-4 lg:items-center">
-      <div className="hidden-section w-full grid justify-start">
-        <SectionHeader number={3} text="Some Things I’ve Built" />
-        <div className="section-main">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-            alias quia earum in blanditiis consectetur quo voluptatem nisi.
-            Aliquam sunt ipsa, molestiae a fugit quidem delectus ducimus quam
-            reiciendis cupiditate non at velit ipsam. Officiis autem magnam
-            voluptas asperiores? Cum, numquam? Deserunt, fugit minus quis dolor
-            reiciendis non culpa. Eveniet, nulla eligendi quae exercitationem
-            ipsam, molestias, enim asperiores reiciendis esse cumque aperiam
-            hic? Quis alias sit nostrum sequi pariatur, ipsum ad, magni
-            repellat, optio totam dignissimos. Sint ea tenetur delectus
-            molestias, doloremque veritatis accusantium obcaecati debitis. Nemo
-            aperiam ducimus dicta veniam, architecto, animi eaque, reprehenderit
-            unde voluptatem assumenda error ab!
-          </p>
-        </div>
+    <div id="portfolio" className="h-screen grid items-center">
+      <div className="hidden-section w-full">
+        <SectionHeader number={3} text="<things i have built>" />
+        <SectionMain>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          accusamus id porro cumque distinctio eos quos, ullam doloremque nam
+          cupiditate sint ea optio, neque suscipit esse est animi eum aperiam
+          quam. Temporibus non, voluptatum ullam deleniti ut numquam, modi ea
+          corporis omnis blanditiis voluptas! Possimus officiis alias quis?
+          Ipsum, nulla.
+        </SectionMain>
+        <SectionFooter text="</things i have built>" />
       </div>
     </div>
   );
