dd5ec9a99d59647f2d4982b58f908d2ecdb819d1
Daniel Dimitrov <79416039+ddimitrov1108@users.noreply.github.com>
2023-02-06 12:09:55 +0200

    .

--- src/components/sections/components/SectionFooter.jsx
+++ src/components/sections/components/SectionFooter.jsx
@@ -6,7 +6,7 @@ export default function SectionFooter({ text = "", className, ...restProps }) {
       className={clsx("section-footer flex justify-end", className)}
       {...restProps}
     >
-      <span className="text-2xl md:text-3xl xl:text-4xl font-bold text-secondary-light/40">
+      <span className="text-2xl md:text-3xl xl:text-4xl font-bold text-secondary-light/60">
         {text}
       </span>
     </div>
