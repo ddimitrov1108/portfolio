4cf7aa44e4fae3b95ba784a8b85c3346098c7d8f
Daniel Dimitrov <79416039+ddimitrov1108@users.noreply.github.com>
2023-02-07 13:12:41 +0200

    .

--- src/components/sections/WelcomeSection.jsx
+++ src/components/sections/WelcomeSection.jsx
@@ -1,5 +1,6 @@
 import { FiLinkedin } from "react-icons/fi";
-import { TypeAnimation } from "react-type-animation";
+
+import StackIcons from "../StackIcons";
 
 export default function WelcomeSection() {
   return (
@@ -8,40 +9,23 @@ export default function WelcomeSection() {
         <div className="text-center lg:text-left">
           <div
             id="me-introduction"
-            className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold"
+            className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold"
           >
             <span>{"Hi! I'm "}</span>
-            <span className="text-transparent bg-clip-text bg-gradient-to-t from-primary-dark via-primary-main to-cyan-400">
-              Daniel.
+            <span className="text-transparent bg-clip-text bg-gradient-to-t from-blue-800 via-primary-main to-cyan-400">
+              Daniel
             </span>
-
+            <span>{", a"}</span>
             <br />
 
-            <div className="w-full">
-              <TypeAnimation
-                sequence={[
-                  1500,
-                  "I'm into Frontend.",
-                  1500,
-                  "I'm into Backend.",
-                  1500,
-                  "I'm into Web design.",
-                  1500,
-                  "I'm a Web Developer.",
-                  2000,
-                ]}
-                speed={35}
-                wrapper="div"
-                cursor={true}
-                repeat={Infinity}
-                className="text-center text-[26px] md:text-5xl lg:text-6xl xl:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-t from-primary-dark via-primary-main to-cyan-400"
-              />
-            </div>
+            <span className="text-transparent bg-clip-text bg-gradient-to-t from-blue-800 via-primary-main to-cyan-400">
+              Web Developer.
+            </span>
           </div>
 
-          <div id="me-description" className="space-y-6 mt-10">
+          <div id="me-description" className="space-y-6 mt-6">
             <p className="mx-auto lg:mx-0 max-w-lg lg:max-w-md xl:max-w-lg text-base">
-              I'm a growing web developer specializing in building and designing
+              I'm a wannabe web developer specializing in building and designing
               stuff for the web. I'm currently a student in search of more
               knowledge and opportunities.
             </p>
@@ -56,7 +40,7 @@ export default function WelcomeSection() {
           </div>
         </div>
 
-        {/* <StackIcons className="hidden lg:block" /> */}
+        <StackIcons className="hidden lg:block -top-12" />
       </div>
     </div>
   );
