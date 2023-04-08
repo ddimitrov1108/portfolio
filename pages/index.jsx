32154c23090433f47e3c9f522031eecd974b49e8
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-04-08 17:02:57 +0300

    final

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ import Footer from "@/components/navigation/Footer";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  style: ["normal", "italic"],
+  style: ["normal"],
   subsets: ["latin"],
 });
 
