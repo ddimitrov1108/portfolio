5e8d0f4e303138e493fc46ee91801e2575acafb4
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-04-08 16:49:09 +0300

    .

--- components/navigation/Footer.jsx
+++ components/navigation/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="absolute bottom-0 right-0 left-0 bg-black/80 w-full">
+    <footer className="bg-black w-full">
 All Rights reserved
     </footer>
   )
