66263e9e7bb83125dfa6d0a8ebe17516f1e0b12d
Daniel Dimitrov <79416039+ddimitrov1108@users.noreply.github.com>
2023-04-10 13:17:44 +0300

    Update Header.jsx

--- components/navigation/Header.jsx
+++ components/navigation/Header.jsx
@@ -88,7 +88,7 @@ export default function Header() {
 
           <Link
             href="/#contact"
-            className="py-2 px-6 bg-primary text-white font-semibold rounded-2xl"
+            className="py-2 px-6 bg-primary text-white font-semibold rounded-full"
           >
             {"Get in touch"}
           </Link>
