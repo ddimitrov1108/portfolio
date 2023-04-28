2a4fee5f90f729244f966d0c7c7f5e046659cf13
Daniel Dimitrov <79416039+ddimitrov1108@users.noreply.github.com>
2023-04-28 16:05:35 +0300

    Update AboutSection.jsx

--- components/sections/AboutSection.jsx
+++ components/sections/AboutSection.jsx
@@ -25,15 +25,12 @@ export default function AboutSection() {
               </h4>
             </SectionHeader>
             <p className="text-secondary">
-              {`I have always been fascinated by the power of the internet to
-              connect people and businesses all over the world. That's why I
-              decided to pursue a career in web development, so I could play a
-              part in making the web a more beautiful and useful place. My
-              expertise lies in React, Tailwind, SCSS, HTML, CSS and JavaScript,
-              and I am always striving to improve my skills and keep up with the
-              latest trends in web development. I am a quick learner and a
-              problem solver, and I love taking on new challenges and finding
-              creative solutions to complex problems.`}
+              {`I have always been fascinated by the power of the internet to connect people and businesses worldwide. 
+              That's why I decided to pursue a career in web development, so I could play a part in making the web a 
+              more beautiful and helpful place. My expertise lies in React, Tailwind, SCSS, HTML, CSS, and JavaScript, 
+              and I am always striving to improve my skills and keep up with the latest trends in web development. 
+              I am a quick learner and a problem solver, and I love taking on new challenges and finding creative 
+              solutions to complex problems.`}
             </p>
           </div>
         </div>
