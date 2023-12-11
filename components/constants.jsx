97c447a1d3f3388f1d3a707b933703192d9df2ba
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-12-11 23:45:36 +0200

    modified:   components/constants.jsx

--- components/constants.jsx
+++ components/constants.jsx
@@ -92,7 +92,7 @@ export const projects = [
   {
     id: uuidv4(),
     view: true,
-    img: "taskdoer-app.webp",
+    img: "taskdoer-app.png",
     url: "https://github.com/ddimitrov1108/taskdoer",
     type: "course work",
     name: "TaskDoer",
