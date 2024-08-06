ba40988ede2806267a770fdc564ba43b5550d57a
Daniel D <79416039+ddimitrov1108@users.noreply.github.com>
2024-08-06 18:08:18 +0300

    modified:   lib/schema.ts

--- lib/schema.ts
+++ lib/schema.ts
@@ -8,18 +8,18 @@ const descriptionRegex = /^$|[\s\S]*/;
 export const contactFormSchema = z.object({
   fullName: z
     .string()
-    .min(3, "Required at least 3 characters long")
-    .max(30, "Required no more than 30 characters long")
+    .min(3, "Required at least 3 characters")
+    .max(30, "Required no more than 30 characters")
     .regex(nameRegex, "Invalid field"),
   email: z
     .string()
     .min(3, "Required to enter an email")
-    .max(80, "Required no more than 80 characters long")
+    .max(80, "Required no more than 80 characters")
     .regex(emailRegex, "Invalid field"),
   subject: z.string().min(1, "Required to select a subject"),
   description: z
     .string()
-    .max(500, "Required no more than 500 characters long")
+    .max(500, "Required no more than 500 characters")
     .regex(descriptionRegex, "Invalid field"),
 });
 
