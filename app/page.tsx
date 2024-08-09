0af00bb01029c5b7fb15a8f4cf8fbdeda88b1786
Daniel D <79416039+ddimitrov1108@users.noreply.github.com>
2024-08-09 19:21:52 +0300

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import AboutSection from "@/components/sections/AboutSection";
-import ContactSection from "@/components/sections/ContactSection";
 import HomeSection from "@/components/sections/HomeSection";
+import AboutSection from "@/components/sections/AboutSection";
+import ProjectsSection from "@/components/sections/ProjectsSection";
+import ContactSection from "@/components/sections/ContactSection";
 
 export default function Home() {
   return (
     <main className="space-y-32">
       <HomeSection />
       <AboutSection />
+      <ProjectsSection />
       <ContactSection />
     </main>
   );
